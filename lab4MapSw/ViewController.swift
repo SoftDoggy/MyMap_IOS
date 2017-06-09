95162c682fe5cb10438c452c8c5b5db453104095
Yijie Yang <774590108@qq.com>
2017-06-09 09:13:26 +0800

    Initial Commit

---
+++ lab4MapSw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab4MapSw
+//
+//  Created by csmacpro01 on 2017/6/9.
+//  Copyright © 2017年 csmacpro01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
